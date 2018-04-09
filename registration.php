7673403874f74037495543625a7bea2c07457177
martin <martin@magextensions.io>
2018-04-09 15:50:23 +0200

    initial commit

---
+++ registration.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Copyright (c) 2017. Magextensions.io - Martin Hooijer. All Rights reserved.
+ * Magextensions License (https://www.magextensions.io/license/)
+ */
+
+\Magento\Framework\Component\ComponentRegistrar::register(
+    \Magento\Framework\Component\ComponentRegistrar::MODULE,
+    'Magextensionsio_JsonFix',
+    __DIR__
+);
